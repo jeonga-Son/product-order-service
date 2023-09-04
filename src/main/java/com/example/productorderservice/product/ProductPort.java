782fa9125557fbbb1a25c421a092f68251ec103f
jeonga-Son <jjeong0106@gmail.com>
2023-09-04 19:55:07 +0900

    상품 조회 기능 구현하기

--- src/main/java/com/example/productorderservice/product/ProductPort.java
+++ src/main/java/com/example/productorderservice/product/ProductPort.java
@@ -2,4 +2,6 @@ package com.example.productorderservice.product;
 
 interface ProductPort { // interface로 만들고 구현은 adapter에서 진행
     void save(Product product);
+
+    Product getProduct(Long productId);
 }
