a24a63f6de4ddb0fa680d993b33309f3c5332499
jeonga-Son <jjeong0106@gmail.com>
2023-09-05 22:03:30 +0900

    상품 주문 기능 : API 테스트로 전환하기

--- src/main/java/com/example/productorderservice/order/OrderService.java
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,8 +1,16 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/orders")
 @Component
 class OrderService {
     private final OrderPort orderPort;
@@ -11,13 +19,16 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(final CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody final CreateOrderRequest request) {
         // 가져온 상품으로 order를 만들어서 order를 저장한다.
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
